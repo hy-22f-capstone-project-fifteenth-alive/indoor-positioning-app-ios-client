66dfa047111eab4e6486e54bd23383d5da2537f0
Jongheon Kim <simba3447@gmail.com>
2023-05-13 15:41:01 +0900

    Monitor beacon region and post suggestion notification

--- IndoorNavigation/AppDelegate.swift
+++ IndoorNavigation/AppDelegate.swift
@@ -12,5 +12,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(2)
+        return true
+    }
 }
 
--- IndoorNavigation/IndoorMapViewController.swift
+++ IndoorNavigation/IndoorMapViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 import CoreLocation
 import MapKit
 import Zip
+import UserNotifications
 
 @available(iOS 15.0, *)
-class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerDelegate {
+class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerDelegate, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
     @IBOutlet var mapView: MKMapView!
     private let locationManager = CLLocationManager()
     @IBOutlet var levelPicker: LevelPickerView!
@@ -23,6 +24,13 @@ class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerD
     private var currentLevelAnnotations = [MKAnnotation]()
     let pointAnnotationViewIdentifier = "PointAnnotationView"
     let labelAnnotationViewIdentifier = "LabelAnnotationView"
+    
+//    let locationManager = CLLocationManager()
+    var foundBeacons = [CLBeacon]()
+    
+    var beaconRegion: CLBeaconRegion!
+    
+    var isRanging = false
     
     // MARK: - View life cycle
     override func viewDidLoad() {
@@ -43,16 +51,7 @@ class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerD
             } catch let error {
                 print(error)
             }
-            
-            // Decode the IMDF data. In this case, IMDF data is stored locally in the current bundle.
-//            let imdfDirectory = Bundle.main.resourceURL!.appendingPathComponent("IMDFData")
-            //        do {
-            //            let imdfDecoder = IMDFDecoder()
-            //            venue = try imdfDecoder.decode(unzipDirectory)
-            //        } catch let error {
-            //            print(error)
-            //        }
-            
+        
             // You might have multiple levels per ordinal. A selected level picker item displays all levels with the same ordinal.
             if let levelsByOrdinal = self.venue?.levelsByOrdinal {
                 let levels = levelsByOrdinal.mapValues { (levels: [Level]) -> [Level] in
@@ -79,6 +78,23 @@ class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerD
             
             // Setup the level picker with the shortName of each level
             setupLevelPicker()
+        }
+        
+        //temp
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            print("Permission granted? \(granted)")
+        }
+        UNUserNotificationCenter.current().delegate = self
+        
+        locationManager.delegate = self // Location Manager 이벤트 핸들러 작성을 위해 CLLocationManagerDelegate 프로토콜 구현체로서 self 객체 할당
+        
+        let uuid = UUID(uuidString: "fda50693-a4e2-4fb1-afcf-c6eb07647825")!
+        beaconRegion = CLBeaconRegion(uuid: uuid, major: 10001, minor: 19641, identifier: uuid.uuidString)
+                
+        if CLLocationManager.authorizationStatus() != .authorizedAlways {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            locationManager.startMonitoring(for: beaconRegion)
         }
     }
     
@@ -250,5 +266,62 @@ class IndoorMapViewController: UIViewController, MKMapViewDelegate, LevelPickerD
         precondition(selectedIndex >= 0 && selectedIndex < self.levels.count)
         let selectedLevel = self.levels[selectedIndex]
         showFeaturesForOrdinal(selectedLevel.properties.ordinal)
+    }
+    
+    // MARK: temp notification configuration
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways:
+            print("status: authorizedAlways")
+            // always permission granted
+        case .authorizedWhenInUse:
+            print("status: authorizedWhenInUse")
+            // when-in-use permission granted
+        default:
+            print("authorisation not granted") // handle appropriately
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        print("Did determine state for region \(region)")
+     
+        switch state {
+        case .inside:
+            postNotification()
+
+            print("Device is within the beacon's range")
+        case .outside:
+            print("Device is outside the beacon's range")
+        case .unknown:
+            print("Beacon’s range is unknown")
+        }
+    }
+    
+    // beacon region 진입 이벤트 핸들러
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        postNotification()
+    }
+    
+    // beacon region 탈출 이벤트 핸들러
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("didExit")
+    }
+    
+    func postNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "한양대학교 제3공학관 진입"
+        content.body = "앱을 통해 실내 공간 정보를 확인하세요."
+        content.sound = UNNotificationSound.default
+        
+        let request = UNNotificationRequest(identifier: "EntryNotification", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
     }
 }
